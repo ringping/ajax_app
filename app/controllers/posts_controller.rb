e5a7a43a8c7aec529bb6bd31303991e09255bd1e
ringping <69758575+ringping@users.noreply.github.com>
2020-10-11 13:12:53 +0900

    AjaxAppの準備

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,12 +1,11 @@
 class PostsController < ApplicationController
 
   def index   #indexアクションを定義した,indexという名前のインスタンスメソッドの定義
-    @posts = Post.all #全レコードをpostsに代入
-  end
-  def new
+    @posts = Post.all.order(id: "DESC") #全レコードをpostsに代入
   end
 
   def create
     Post.create(content: params[:content])
+    redirect_to action: :index
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  get 'posts', to: 'posts#index'  #postsというパスに、GETメソッドでリクエストが送られてきたとき、postsコントローラーの、index、indexアクションを呼び出す
-  get 'posts/new', to: 'posts#new'
+ root to: 'posts#index'
   post 'posts', to: 'posts#create'
 end
